4e2f4c98cc37be021ae3b941ff8bccc0bb8a90e1
sanekb <sasha.mostimenko@yandex.ru>
2026-06-05 10:41:07 +0300

    chore: static font

--- src/server/app.js
+++ src/server/app.js
@@ -23,10 +23,8 @@ const html = (page, state) => `
         <title>chess-obs</title>
   		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<link rel="icon" type="image/svg+xml" href="/favicon.svg">
+		<link rel="preload" href="/montserrat-wght.woff2" as="font" type="font/woff2" crossorigin>
         <link rel="stylesheet" href="/app.css">
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     </head>
     <body>
         <script id="init-data" type="application/json">
